6799733acb7643fa06e9388fa2ab3b9b185c16da
shafreen <shafreen@wso2.com>
2017-08-08 11:08:50 +0530

    Introduce new interfaces

--- connector-framework/src/main/java/org/wso2/carbon/connector/framework/http/HTTPMessage.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.connector.framework.http;
-
-/**
- * Represents an inbound http request.
- */
-public class HTTPMessage {
-}
--- connector-framework/src/main/java/org/wso2/carbon/connector/framework/http/Observer.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.connector.framework.http;
-
-/**
- * Allows to get notifications.
- */
-public interface Observer {
-    void update(HTTPMessage httpMessage, Observer callback);
-}
