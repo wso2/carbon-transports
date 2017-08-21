ba1e483bb507a03a10e795c365afe486ee833734
Rajith <lanka.vitharana@gmail.com>
2017-08-21 17:28:40 +0530

    Add javadoc comments

--- jms/org.wso2.carbon.transport.jms/src/main/java/org/wso2/carbon/transport/jms/contract/JMSListener.java
+++ jms/org.wso2.carbon.transport.jms/src/main/java/org/wso2/carbon/transport/jms/contract/JMSListener.java
@@ -7,6 +7,17 @@ import org.wso2.carbon.transport.jms.callback.JMSCallback;
  * Allows to get notifications.
  */
 public interface JMSListener {
+    /**
+     * Transport will trigger this method when for each jms message.
+     *
+     * @param jmsMessage contains the msg data.
+     */
     void onMessage(CarbonMessage jmsMessage, JMSCallback jmsCallback);
+
+    /**
+     * If there are errors, transport will trigger this method.
+     *
+     * @param throwable contains the error details of the event.
+     */
     void onError(Throwable throwable);
 }
