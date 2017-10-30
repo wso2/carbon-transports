9fe1dd8504bc926385e12796e52726c44c7c1494
wggihan <wggihan@gmail.com>
2017-10-30 14:36:32 +0530

    Add Custom Thread Factory to provide name for threads

--- file/org.wso2.carbon.transport.remote-file-system/src/main/java/org/wso2/carbon/transport/remotefilesystem/server/util/ThreadPoolFactory.java
+++ file/org.wso2.carbon.transport.remote-file-system/src/main/java/org/wso2/carbon/transport/remotefilesystem/server/util/ThreadPoolFactory.java
@@ -21,7 +21,9 @@ package org.wso2.carbon.transport.remotefilesystem.server.util;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A Thread Pool factory for creating Thread Pools required for the File System transport
@@ -31,9 +33,9 @@ public class ThreadPoolFactory {
 
     public ThreadPoolFactory(int threadPoolSize) {
         if (threadPoolSize == 0) {
-            executorService = Executors.newSingleThreadExecutor();
+            executorService = Executors.newSingleThreadExecutor(new CustomThreadFactory());
         } else {
-            executorService = Executors.newFixedThreadPool(threadPoolSize);
+            executorService = Executors.newFixedThreadPool(threadPoolSize, new CustomThreadFactory());
         }
     }
 
@@ -57,6 +59,38 @@ public class ThreadPoolFactory {
             }
         } catch (InterruptedException e) {
             executorService.shutdownNow();
+        }
+    }
+
+    /**
+     * Custom thread factory class implementation.
+     * This will allow us to provide a name for threads.
+     * Edited original java.util.concurrent.DefaultThreadFactory implementation.
+     */
+    static class CustomThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        CustomThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = "remote-file-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
         }
     }
 }
